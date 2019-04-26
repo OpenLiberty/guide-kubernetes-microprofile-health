514d5ff99b3765a46f33fa74f2868d4256c427a0
Panagiotis Roubatsis <proubatsis@users.noreply.github.com>
2019-04-26 16:24:57 -0400

    Inventory/System conversion (#48)
    * Replace name/ping with inventory/system
    
    * Augment inventory/system with mpHealth features from name/ping
    
    * Convert README to refer to inventory/system instead of name/ping
    
    * Use two system replicas
    
    * Update travis script
    
    * Apply feedback from Acrolinx
    
    * Fix display issues
    
    * Change ping comment to inventory in kubernetes.yaml
    
    * Use POST request to system-service to determine if inventory is healthy
    
    * Respect the line limit in InventoryHealth
    
    * Clarify that the system pods will become available after 60 seconds

---
+++ start/system/src/main/java/io/openliberty/guides/system/UnhealthyResource.java
@@ -0,0 +1,25 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::copyright[]
+package io.openliberty.guides.system;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("unhealthy")
+public class UnhealthyResource {
+    @POST
+    public String unhealthy() {
+        SystemHealth.setUnhealthy();
+        return System.getenv("HOSTNAME") + " is now unhealthy...\n";
+    }
+}
