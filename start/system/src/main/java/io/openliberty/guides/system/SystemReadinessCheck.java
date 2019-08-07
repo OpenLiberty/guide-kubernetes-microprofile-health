e45216345b83f83f8dd2e02655144622cb290c6b
Andrew <andrew.deschenes@ibm.com>
2019-08-06 21:44:55 -0400

    Updating start directory

---
+++ start/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
@@ -0,0 +1,44 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::copyright[]
+package io.openliberty.guides.system;
+
+import java.time.LocalDateTime;
+	
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Readiness
+@ApplicationScoped
+public class SystemReadinessCheck implements HealthCheck {
+	private static final int ALIVE_DELAY_SECONDS = 60;
+	private static LocalDateTime aliveAfter = LocalDateTime.now();
+
+	@Override
+	public HealthCheckResponse call() {
+		if (isAlive()) {
+			return HealthCheckResponse.named("isAlive").up().build();
+		}
+
+		return HealthCheckResponse.named("isAlive").down().build();
+	}
+
+	public static void setUnhealthy() {
+		aliveAfter = LocalDateTime.now().plusSeconds(ALIVE_DELAY_SECONDS);
+	}
+
+	private static boolean isAlive() {
+		return LocalDateTime.now().isAfter(aliveAfter);
+	}
+}
