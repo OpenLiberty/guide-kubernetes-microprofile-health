667878656983756ef66088e7986404be9965a098
DannyS03 <danny.salman@ryerson.ca>
2020-04-23 13:33:00 -0400

    addressing feedback

--- start/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
+++ start/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,10 +29,10 @@ public class SystemReadinessCheck implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
         if (isAlive()) {
-            return HealthCheckResponse.builder().name("isAlive").up();
+            return HealthCheckResponse.up("isAlive");
         }
 
-        return HealthCheckResponse.builder().name("isAlive").down();
+        return HealthCheckResponse.down("isAlive");
     }
 
     public static void setUnhealthy() {
