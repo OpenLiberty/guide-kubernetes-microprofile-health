31131aca9696f029ef734de811a6301b3a90aedc
DannyS03 <danny.salman@ryerson.ca>
2020-04-23 01:54:00 -0400

    updating readiness healthcheck

--- finish/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
+++ finish/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
@@ -10,36 +10,36 @@
  *     IBM Corporation - Initial implementation
  *******************************************************************************/
  // end::copyright[]
-package io.openliberty.guides.system;
+ package io.openliberty.guides.system;
 
-import java.time.LocalDateTime;
-
-import javax.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.health.Readiness;
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-
-@Readiness
-@ApplicationScoped
-public class SystemReadinessCheck implements HealthCheck {
-    
-    private static final int ALIVE_DELAY_SECONDS = 60;
-    private static LocalDateTime aliveAfter = LocalDateTime.now();
-
-    @Override
-    public HealthCheckResponse call() {
-        if (isAlive()) {
-            return HealthCheckResponse.builder().name("isAlive").up();
-        }
-
-        return HealthCheckResponse.builder().name("isAlive").down();
-    }
-
-    public static void setUnhealthy() {
-        aliveAfter = LocalDateTime.now().plusSeconds(ALIVE_DELAY_SECONDS);
-    }
-
-    private static boolean isAlive() {
-        return LocalDateTime.now().isAfter(aliveAfter);
-    }
-}
+ import java.time.LocalDateTime;
+ 
+ import javax.enterprise.context.ApplicationScoped;
+ import org.eclipse.microprofile.health.Readiness;
+ import org.eclipse.microprofile.health.HealthCheck;
+ import org.eclipse.microprofile.health.HealthCheckResponse;
+ 
+ @Readiness
+ @ApplicationScoped
+ public class SystemReadinessCheck implements HealthCheck {
+     
+     private static final int ALIVE_DELAY_SECONDS = 60;
+     private static LocalDateTime aliveAfter = LocalDateTime.now();
+ 
+     @Override
+     public HealthCheckResponse call() {
+         if (isAlive()) {
+             return HealthCheckResponse.up("isAlive");
+         }
+ 
+         return HealthCheckResponse.up("isAlive");
+     }
+ 
+     public static void setUnhealthy() {
+         aliveAfter = LocalDateTime.now().plusSeconds(ALIVE_DELAY_SECONDS);
+     }
+ 
+     private static boolean isAlive() {
+         return LocalDateTime.now().isAfter(aliveAfter);
+     }
+ }
