273a38a5315c97f9ae6b8a15f9e38be36fc4b39a
Gilbert Kwan <gkwan@ca.ibm.com>
2021-08-11 10:08:07 -0400

    Update SystemEndpointIT.java

--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2018, 2019 IBM Corporation and others.
+ * Copyright (c) 2018, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,9 +38,8 @@ public class SystemEndpointIT {
 
     @BeforeAll
     public static void oneTimeSetup() {
-        String clusterIp = System.getProperty("cluster.ip");
-        String nodePort = System.getProperty("system.node.port");
-        clusterUrl = "http://" + clusterIp + ":" + nodePort + "/system/properties/";
+        String systemRootPath = System.getProperty("system.service.root");
+        clusterUrl = "http://" + systemRootPath + "/system/properties/";
     }
     
     @BeforeEach
