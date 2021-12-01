7197d46c2cf950ecdac543e54b725a8bf6b2d0f9
Kevin Cho <Kevcpro@users.noreply.github.com>
2021-12-01 09:38:41 -0500

    Update SystemStartupCheck.java

--- finish/system/src/main/java/io/openliberty/guides/system/SystemStartupCheck.java
+++ finish/system/src/main/java/io/openliberty/guides/system/SystemStartupCheck.java
@@ -26,19 +26,16 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 @ApplicationScoped
 public class SystemStartupCheck implements HealthCheck {
 
-    private static final String STARTUP_CHECK = SystemResource.class.getSimpleName()
-                                               + " Startup Check";
-
     @Override
     public HealthCheckResponse call() {
         OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)
         ManagementFactory.getOperatingSystemMXBean();
         long cpuUsed = bean.getSystemCpuLoad();
-        if (cpuUsed < 0.95) {
-           return HealthCheckResponse.up(STARTUP_CHECK).withData("cpu used", cpuUsed);
-        } else {
-           return HealthCheckResponse.down(STARTUP_CHECK).withData("cpu used", cpuUsed);
-        }
+        
+        return HealthCheckResponse.named(SystemResource.class
+                                            .getSimpleName() + " Liveness Check")
+                                            .withData("cpu used", cpuUsed)
+                                            .status(cpuUsed < 0.95).build();
     }
 }
 
