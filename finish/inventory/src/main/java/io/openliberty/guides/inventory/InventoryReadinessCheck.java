e0646bc11c2f4cf57ee5d395aa85f11c54209d1d
Gilbert Kwan <gkwan@ca.ibm.com>
2022-01-06 10:31:05 -0500

    Update InventoryReadinessCheck.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryReadinessCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryReadinessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2019, 2021 IBM Corporation and others.
+ * Copyright (c) 2019, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,9 @@ import org.eclipse.microprofile.health.Readiness;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
+// tag::Readiness[]
 @Readiness
+// tag::Readiness[]
 @ApplicationScoped
 public class InventoryReadinessCheck implements HealthCheck {
 
