e1ff02103486dd07af12476a59d7e17500289382
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-17 13:38:35 -0500

    linter fixes

--- start/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
+++ start/system/src/main/java/io/openliberty/guides/system/SystemReadinessCheck.java
@@ -29,10 +29,12 @@ public class SystemReadinessCheck implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
         if (isAlive()) {
-            return HealthCheckResponse.up(SystemResource.class.getSimpleName() + "Readiness Check");
+            return HealthCheckResponse.up(SystemResource.class.getSimpleName()
+                                          + "Readiness Check");
         }
 
-        return HealthCheckResponse.down(SystemResource.class.getSimpleName() + "Readiness Check");
+        return HealthCheckResponse.down(SystemResource.class.getSimpleName()
+                                        + "Readiness Check");
     }
 
     public static void setUnhealthy() {
