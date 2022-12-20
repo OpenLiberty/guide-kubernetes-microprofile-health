9502449b65b09909012d3f49ff49315f9972fc0b
jakub-pomykala <56261244+jakub-pomykala@users.noreply.github.com>
2022-12-20 11:22:40 -0500

    adding -ntp to maven (#135)
    * adding -ntp to maven
    
    * Fixing broken builds
    
    * Update test.yml
    
    * fixing daily build scripts + testing
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -9,15 +9,25 @@ set -euxo pipefail
 
 # Test app
 
-mvn -Dhttp.keepAlive=false \
+#../scripts/startMinikube.sh
+minikube start
+minikube status
+#kubectl cluster-info
+#kubectl get services --all-namespaces
+#kubectl config view
+eval "$(minikube docker-env)"
+
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q package
 
-docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
+docker pull -q icr.io/appcafe/open-liberty:full-java11-openj9-ubi
 
 docker build -t system:1.0-SNAPSHOT system/.
 docker build -t inventory:1.0-SNAPSHOT inventory/.
+
+docker images
 
 kubectl apply -f kubernetes.yaml
 
@@ -27,16 +37,20 @@ kubectl get pods
 
 minikube ip
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -Ddockerfile.skip=true \
     -Dsystem.service.root="$(minikube ip):31000" -Dinventory.service.root="$(minikube ip):32000" \
     failsafe:integration-test
-mvn failsafe:verify
+mvn -ntp failsafe:verify
 
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system | head -1)"
 
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system | tail -1)"
 
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
+
+# sudo ../scripts/stopMinikube.sh
+eval "$(minikube docker-env -u)"
+minikube stop
